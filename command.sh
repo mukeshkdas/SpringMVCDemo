60eab37781b64a3339adc1f6d76cd24e7ecf3064
Mukesh Kumar Das <mukeshk_das@infosys.com>
2018-04-16 21:50:24 +0530

    added

--- command.sh
+++ command.sh
@@ -3,3 +3,6 @@ docker run -p 9000:9000 --name sonarqube sonarqube:6.3.1
 
 docker pull docker.bintray.io/jfrog/artifactory-oss:5.4.4
 docker run -p 8081:8081 --name artifactory docker.bintray.io/jfrog/artifactory-oss:5.4.4
+
+
+docker pull sonarqube:6.2
